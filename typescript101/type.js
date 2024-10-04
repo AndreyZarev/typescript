c5d07a886e5f94645b08237e0488097e326ccfe5
tocode <andrey.zarev@outlook.com>
2024-10-04 23:57:52 +0300

    Update type.js

--- typescript101/type.js
+++ typescript101/type.js
@@ -3,3 +3,7 @@ var arr = ['asds', 'asd', 'dsda'];
 arr.forEach(function (arr) {
     console.log(arr);
 });
+var arrNum = [1, 2, 3, 4, 5];
+console.log(arrNum);
+var bulArray = [true, false, true, false];
+console.log(bulArray);
