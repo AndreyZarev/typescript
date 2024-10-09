4a7cf272920fa688d4f1752f379eac55da3bda94
tocode <andrey.zarev@outlook.com>
2024-10-09 16:09:29 +0300

    Update colorie-object.js

--- 2lab/colorie-object.js
+++ 2lab/colorie-object.js
@@ -1,16 +1,7 @@
 function colorieObj(foods) {
     var foodNames = {};
-    var element = [];
-    var i = 0;
-    for (var index = 0; index < foods.length; index++) {
-        if (index % 2 === 0) {
-            element[i] = foods[index];
-            foodNames[foods[index]] = "";
-        }
-        else {
-            var food = element[i - 1];
-            foodNames.key = foods[index - 1];
-        }
+    for (var index = 0; index <= foods.length; index + 2) {
+        foodNames[foods[index]] = foods[index + 1];
     }
     return foodNames;
 }
