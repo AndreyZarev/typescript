327314ffd9629077c54a97f62a5ef3d619f75e22
tocode <andrey.zarev@outlook.com>
2024-10-09 16:01:18 +0300

    Update colorie-object.ts

---
+++ 2lab/colorie-object.ts
@@ -0,0 +1,22 @@
+function colorieObj(foods: Array<string>): Object {
+
+    let foodNames: { [key: string]: String } = {};
+    let element: String[] = [];
+    let i = 0
+    for (let index = 0; index < foods.length; index++) {
+
+        if (index % 2 === 0) {
+            element[i] = foods[index]
+            foodNames[foods[index]] = ""
+        } else {
+            let food = element[i - 1]
+            foodNames.key = foods[index - 1]
+        }
+    }
+
+    return foodNames
+}
+
+
+colorieObj(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
+// { Yoghurt: 48, Rise: 138, Apple: 52 }
