bdb7f7882d318e7353c0ac6dda2098a0eb781d99
tocode <andrey.zarev@outlook.com>
2024-10-09 16:00:39 +0300

    Update ex4.ts

--- lab1/ex4/ex4.ts
+++ lab1/ex4/ex4.ts
@@ -1,12 +1,13 @@
 function evenPossitions(arr: Array<string>) {
-    let sum = 0
+    let sum = ""
     for (let index = 0; index < arr.length; index++) {
         if (index % 2 === 0) {
-            let element = Number(arr[index])
-            sum += element
+            let element = arr[index]
+            sum += element + " "
         }
 
     }
+    sum = sum.trim()
     console.log(sum);
 
     return sum
