465216f8165181f34206fff25d717f37c7538e9d
tocode <andrey.zarev@outlook.com>
2024-10-10 14:32:13 +0300

    Delete colorie-object.js

--- 2lab/colorie-object.js
+++
@@ -1,9 +0,0 @@
-function colorieObj(foods) {
-    var foodNames = {};
-    for (var index = 0; index <= foods.length; index + 2) {
-        foodNames[foods[index]] = foods[index + 1];
-    }
-    return foodNames;
-}
-console.log(colorieObj(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
-// { Yoghurt: 48, Rise: 138, Apple: 52 }
