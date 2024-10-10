fbe5dd2c23d1c0189f5a4175eae300a12e5c2284
tocode <andrey.zarev@outlook.com>
2024-10-10 14:32:05 +0300

    Create colorie-object.ts

---
+++ 2lab/1ex/colorie-object.ts
@@ -0,0 +1,30 @@
+function colorieObj(foods: Array<string>): Object {
+
+    let foodNames: { [key: string]: number } = {};
+
+    for (let index = 0; index <= foods.length; index = index + 2) {
+
+        if (foods.length <= index) {
+            return foodNames
+
+        }
+        let num: number = Number(foods[index + 1])
+
+        foodNames[foods[index]] = num
+
+    }
+
+    return foodNames
+}
+
+
+console.log(
+    colorieObj(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
+);
+// { Yoghurt: 48, Rise: 138, Apple: 52 }
+
+console.log(
+    colorieObj(
+        ['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42'])
+);
+// { Potato: 93, Skyr: 63, Cucumber: 18, Milk: 42 }
