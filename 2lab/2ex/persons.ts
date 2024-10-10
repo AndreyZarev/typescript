a5220004e932b4efaaaa720c4fcfedf0976bf125
tocode <andrey.zarev@outlook.com>
2024-10-10 14:32:09 +0300

    Create persons.ts

---
+++ 2lab/2ex/persons.ts
@@ -0,0 +1,20 @@
+function persons(firstName: string, lastName: string, age: string): object {
+    let obj: { [key: string]: string } = {};
+
+    obj["firstName"] = firstName
+    obj["lastName"] = lastName
+    obj["age"] = age
+
+    return obj
+}
+
+
+console.log(
+    persons("Peter", "Pan", "20")
+);
+// firstName: Peter lastName: Pan age: 20
+console.log(
+
+    persons("Peter", "Pan", "20")
+);
+// { Potato: 93, Skyr: 63, Cucumber: 18, Milk: 42 }
