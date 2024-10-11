71015127f068ec1620fb56542b924e769113fdd5
tocode <andrey.zarev@outlook.com>
2024-10-11 14:50:26 +0300

    Update inventory.ts

---
+++ 2lab/3ex/inventory.ts
@@ -0,0 +1,48 @@
+function inventory(arr: string[]) {
+
+    let resultArray: Array<string[]> = [];
+    for (let index = 0; index < arr.length; index++) {
+        const [hero, level, items] = arr[index].split("/");
+        let array = `${hero}/ ${level}/ ${items}`.split("/")
+        // `Hero: ${hero}/ Level => ${level}/ Items => ${items}`.split("/")
+
+
+        resultArray.push(array);
+
+        // resultArray.push(`Hero: ${hero}`)
+        // resultArray.push(`Level => ${level}`)
+        // resultArray.push(`Items => ${items}`)
+
+
+    }
+
+    resultArray = resultArray.sort((a, b) => {
+        const numA = parseInt(a[1].trim(), 10); // Trim and convert to number
+        const numB = parseInt(b[1].trim(), 10);
+        return numA - numB; // Sort in ascending order
+    });
+    let final = resultArray.join(",").split(",");
+
+    return final
+}
+
+
+
+
+
+console.log(inventory([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+]
+));
+
+// Hero: Hes
+// level => 1
+// items => Desolator, Sentinel, Antara
+// Hero: Derek
+// level => 12
+// items => BarrelVest, DestructionSword
+// Hero: Isacc
+// level => 25
+// items => Apple, GravityGun
