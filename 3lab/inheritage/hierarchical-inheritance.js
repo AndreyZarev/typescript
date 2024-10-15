0ffe02db012e74ea5679c883e71245402ec57b97
tocode <andrey.zarev@outlook.com>
2024-10-16 00:09:30 +0300

    Create hierarchical-inheritance.js

---
+++ 3lab/inheritage/hierarchical-inheritance.js
@@ -0,0 +1,51 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Animal = /** @class */ (function () {
+    function Animal() {
+        this.eat = function () {
+            console.log("eating…");
+        };
+    }
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.bark = function () {
+            console.log("barking…");
+        };
+        return _this;
+    }
+    return Dog;
+}(Animal));
+var Cat = /** @class */ (function (_super) {
+    __extends(Cat, _super);
+    function Cat() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.meow = function () {
+            console.log("meowing…");
+        };
+        return _this;
+    }
+    return Cat;
+}(Animal));
+var dog = new Dog();
+dog.eat();
+dog.bark();
+var cat = new Cat();
+cat.eat();
+cat.meow();
