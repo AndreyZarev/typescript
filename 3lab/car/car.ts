b491c96a4c387bf8c8fca9bf8e0629d1677f70c2
tocode <andrey.zarev@outlook.com>
2024-10-15 14:23:51 +0300

    Create car.ts

---
+++ 3lab/car/car.ts
@@ -0,0 +1,39 @@
+
+class Car {
+    private name: string;
+    private model: string;
+    private hp: number;
+    // name: string, model: string, hp: number
+    constructor() {
+
+    }
+    get getName() { return this.name; }
+    get getModel() { return this.model; }
+    get getHp() { return this.hp; }
+
+    set setName(name: string) { this.name = name; }
+    set setModel(model: string) { this.model = model; }
+    set setHp(hp: number) { this.hp = hp; }
+
+}
+
+
+const car1 = new Car();
+const car2 = new Car();
+
+car1.setName = "Chevrolet"
+car1.setModel = "Impala"
+car1.setHp = 390
+
+car2.setName = "Skoda"
+car2.setModel = "Karoq"
+car2.setHp = 150
+
+console.log(`The car is: ${car1.getName} ${car1.getModel} - ${car1.getHp}.`);
+console.log(`The car is: ${car2.getName} ${car2.getModel} - ${car2.getHp}.`);
+
+// Chevrolet Impala 390
+// The car is: Chevrolet Impala - 390 HP.
+
+//
+// The car is: This Skoda Karoq - 150 HP.
