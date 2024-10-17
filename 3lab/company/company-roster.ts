ed09967ff6087beb56e9a4f21013759560d615d9
tocode <andrey.zarev@outlook.com>
2024-10-17 15:39:30 +0300

    Update company-roster.ts

--- 3lab/company/company-roster.ts
+++ 3lab/company/company-roster.ts
@@ -4,7 +4,7 @@ class Department {
     position: string;
     department: string;
     email?: string;
-    age?: number;
+    age?: string;
 
     constructor(
         name: string,
@@ -12,7 +12,7 @@ class Department {
         position: string,
         department: string,
         email?: string,
-        age?: number) {
+        age?: string) {
 
 
         this.name = name;
@@ -27,21 +27,23 @@ class Department {
 }
 
 
-function departmentsPay(n: number, arr: string[]) {
+function departmentsPay(n: number, arr: Array<string[]>) {
     let final = []
 
     for (let index = 0; index < n; index++) {
-        const employee = arr[index].split(' ');
-        // const department = new Department(employee)
+        let l = arr[index]
+        //     let [name, salary, position, department, email, age] = arr[index]
+        //     const classI = new Department(name, salary, position, department, email, age)
+        // }
+
     }
 
 }
 
+departmentsPay(4, [["Peter", "120.00", "Dev", "Development", "peter@abv.bg", "28"],
+["Tina", '333.33', "Manager", "Marketing", '33'],
 
-
-departmentsPay(4, ["Peter 120.00 Dev Development peter@abv.bg 28",
-    "Tina 333.33 Manager Marketing 33",
-    "Sam 840.20 ProjectLeader Development sam@sam.com"])
+["Sam", '840.20', "ProjectLeader", "Development", "sam@sam.com"]])
 
 
 // Highest Average Salary: Development
