c63cab313f45b1d7fe2a3b1069b18856cfc4bf3a
tocode <andrey.zarev@outlook.com>
2024-10-17 15:39:33 +0300

    Update company-roster.js

--- 3lab/company/company-roster.js
+++ 3lab/company/company-roster.js
@@ -12,13 +12,15 @@ var Department = /** @class */ (function () {
 function departmentsPay(n, arr) {
     var final = [];
     for (var index = 0; index < n; index++) {
-        var employee = arr[index].split(' ');
-        // const department = new Department(employee)
+        var l = arr[index];
+        //     let [name, salary, position, department, email, age] = arr[index]
+        //     const classI = new Department(name, salary, position, department, email, age)
+        // }
     }
 }
-departmentsPay(4, ["Peter 120.00 Dev Development peter@abv.bg 28",
-    "Tina 333.33 Manager Marketing 33",
-    "Sam 840.20 ProjectLeader Development sam@sam.com"]);
+departmentsPay(4, [["Peter", "120.00", "Dev", "Development", "peter@abv.bg", "28"],
+    ["Tina", '333.33', "Manager", "Marketing", '33'],
+    ["Sam", '840.20', "ProjectLeader", "Development", "sam@sam.com"]]);
 // Highest Average Salary: Development
 // George 0.20 Freeloader Nowhere 18
 // Sam 840.20 sam@sam.com -1
