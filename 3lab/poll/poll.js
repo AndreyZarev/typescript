f04455c1ac28972a823aa473b2b22d412fdd1b2c
tocode <andrey.zarev@outlook.com>
2024-10-23 18:22:04 +0300

    Update poll.js

--- 3lab/poll/poll.js
+++ 3lab/poll/poll.js
@@ -1,11 +1,10 @@
-var Person1 = /** @class */ (function () {
-    function Person1(name, age) {
+class Person1 {
+    constructor(name, age) {
         this.name = name;
         this.age = age;
     }
-    return Person1;
-}());
-var person = new Person1("Peter", 12);
-console.log("".concat(person.name, " is ").concat(person.age, " years old."));
+}
+const person = new Person1("Peter", 12);
+console.log(`${person.name} is ${person.age} years old.`);
 // Peter 12
 // Peter is 12 years old.
