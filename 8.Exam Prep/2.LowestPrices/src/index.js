50d0cd6e39d974b2ece40bd92909eca3daee30b0
tocode <andrey.zarev@outlook.com>
2024-10-24 17:25:52 +0300

    Create index.js

---
+++ 8.Exam Prep/2.LowestPrices/src/index.js
@@ -0,0 +1,43 @@
+const inputCollection = [
+    "New York | Sample Product | 1000.1",
+    "Sample Town | Sample Product | 1000",
+    "Sofia | Orange | 3",
+    "Sample Town | Orange | 2",
+    "Sample Town | Peach | 1",
+    "Sofia | Peach | 2",
+    "New York | Burger | 10",
+];
+const generateProductTownPriceMap = (input) => {
+    const result = {};
+    for (const line of input) {
+        const [town, product, price] = line.split(" | ");
+        const parsedPrice = Number(price);
+        if (!result[product]) {
+            result[product] = {};
+        }
+        if (!result[product][town]) {
+            result[product][town] = parsedPrice;
+        }
+        else {
+            if (result[product][town] > parsedPrice) {
+                result[product][town] = parsedPrice;
+            }
+        }
+    }
+    return result;
+};
+const printResult = (result) => {
+    const tuples = Object.entries(result);
+    for (const [product, townPriceMap] of tuples) {
+        const townPricesTuple = Object.entries(townPriceMap);
+        const sorted = townPricesTuple.sort((a, b) => a[1] - b[1]);
+        const [town, price] = sorted[0];
+        // Sample Product -> 1000 (Sample Town)
+        console.log(`${product} -> ${price} (${town})`);
+    }
+};
+function solve(input) {
+    const result = generateProductTownPriceMap(input);
+    printResult(result);
+}
+solve(inputCollection);
