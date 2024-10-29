95e9503270c23f9936e438862896f86603f2cc1b
tocode <andrey.zarev@outlook.com>
2024-10-29 19:18:12 +0200

    Update ex3.js

--- lab1/ex3/ex3.js
+++ lab1/ex3/ex3.js
@@ -1,9 +1,9 @@
 function sumBetween(num1, num2) {
-    var sum = 0;
-    var num1N = Number(num1);
-    var num2N = Number(num2);
-    for (var index = num1N; index <= num2N; index++) {
-        var value = index;
+    let sum = 0;
+    let num1N = Number(num1);
+    let num2N = Number(num2);
+    for (let index = num1N; index <= num2N; index++) {
+        let value = index;
         sum += value;
     }
     console.log(sum);
