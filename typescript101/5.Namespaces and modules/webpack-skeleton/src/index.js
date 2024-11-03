834d5203e52a4896a5af60b88e196e0437e62c81
tocode <andrey.zarev@outlook.com>
2024-11-03 15:51:02 +0200

    Create index.js

---
+++ typescript101/5.Namespaces and modules/webpack-skeleton/src/index.js
@@ -0,0 +1,21 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const p = new Person("Mitko");
+console.log(p.name);
+class Person2 {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const p1 = new Person2("Maria");
+console.log(p1.name);
+class Person3 {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const p3 = new Person("Pesho");
+console.log(p3.name);
