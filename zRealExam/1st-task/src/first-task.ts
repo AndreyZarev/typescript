c059ff16ced41d2056afc1065d0a6ddce5fc7331
tocode <andrey.zarev@outlook.com>
2024-11-10 15:24:10 +0200

    Create first-task.ts

---
+++ zRealExam/1st-task/src/first-task.ts
@@ -0,0 +1,26 @@
+function printNum(num1: number, num2: number): string {
+    let sum: number = 0;
+    let str: string = "";
+    for (let index = num1; index <= num2; index++) {
+        str += index + " ";
+        sum += index
+
+    }
+
+    let result = `${str.trim()}\nSum: ${sum}`
+    return result;
+}
+
+console.log(printNum(5, 10));
+
+//5 6 7 8 9 10
+//Sum: 45
+
+console.log(printNum(0, 26));
+//0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 
+//Sum: 351
+
+console.log(printNum(50, 60));
+
+//50 51 52 53 54 55 56 57 58 59 60
+//Sum: 605
